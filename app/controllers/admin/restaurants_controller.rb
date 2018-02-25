4208d98a731326466a6be1da3c92b4008f3a7c30
Jason <jacklin953107@gmail.com>
2018-02-25 14:39:35 +0800

    add restaurant new and create actions and views for admin

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -3,6 +3,19 @@ class Admin::RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
+  def new
+    @restaurant = Restaurant.new
+  end
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      flash[:notice] = "餐廳成功建立"
+      redirect_to admin_restaurants_path
+    else
+      flash[:alert] = "餐廳建立失敗"
+      render :new
+    end
+  end
 
   private
   def authenticate_admin
@@ -10,5 +23,8 @@ class Admin::RestaurantsController < ApplicationController
       flash[:alert] = "Not allow"
       redirect_to root_path
     end
+  end
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :tel, :address, :opening_hours, :description)
   end
 end
