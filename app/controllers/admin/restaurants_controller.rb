6093684903ddff067b0e4eb8a9e95ba0ad91db1a
Jason <jacklin953107@gmail.com>
2018-02-26 23:17:43 +0800

    add Admin::BaseController and move authenticate_admin here

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,5 +1,5 @@
-class Admin::RestaurantsController < ApplicationController
-  before_action :authenticate_admin
+class Admin::RestaurantsController < Admin::BaseController
+
   before_action :find_restaurant, only: [:show, :edit, :update, :destroy]
   def index
     @restaurants = Restaurant.page(params[:page]).per(10)
